0494b6a93d95fdb9de9ff8b7df3e24d9533f6b47
prakriti5dhang <prakritidhang@gmail.com>
2023-10-04 11:51:25 +0530

    emsa simulation updated

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -205,7 +205,7 @@ function sample_dilutionA(){
   document.getElementById("dnastrand1").style.display="none";
   document.getElementById("emptytube").style.display="none";
   document.getElementById("action").innerHTML = "1x dilution is selected ";
-  document.getElementById("step3").disabled = false;
+  document.getElementById("step3").disabled = true;
  
   
 }
